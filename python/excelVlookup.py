a3517daecb4408c1148ea3dd7ce2d99a716865da
Jing Hua <hwajing@outlook.jp>
2019-07-25 16:11:44 +0800

    Update excelVlookup.py

--- python/excelVlookup.py
+++ python/excelVlookup.py
@@ -1,20 +1,22 @@
-import pandas as pd
-import os
-from openpyxl import load_workbook
-# ===============================================================================================================
 # 技术文档
 # 【pandas】
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.merge.html?highlight=merge#pandas.merge
 # 【openpyxl】
 # https://openpyxl.readthedocs.io/en/stable/api/openpyxl.workbook.workbook.html
 # ===============================================================================================================
-# ★★★请自行修改相应参数★★★
+import pandas as pd
+import os
+from openpyxl import load_workbook
+# ===============================================================================================================
+# ★★★★★★★★★★★★★★★★★★★★★★★★★参数区域，请自行修改★★★★★★★★★★★★★★★★★★★★★★★★★★
 # 【filePath】文件路径，不包含文件名
 # 【fileName】文件名（含扩展名）
 # 【sheetLeft】【sheetRight】2个sheet名，以Left表为基准，将Right表的数据匹配过来。
 # 【joinColumns】匹配列，左右表必须同名
 # 【resultColumns】为Right表中挑选列（可指定多列，但必须包含匹配列）。显示结果为左表所有列+右表挑选列。
-# 【fileName4Result】如需在新文件中显示结果，此参数指定新文件名（如与原文件名一致则在原文件内添加sheet）
+# 【fileName4Result】显示匹配后结果的文件名（如与原文件名一致则在原文件内添加sheet）
+# 【sheetName4Result】显示匹配后结果的sheet名（如有重复，则覆盖）
+# 【how】连接方式（一般vlookup操作为left）
 filePath = 'C:/Users/hwaji/PycharmProjects/Arduino/excel/'
 fileName = 'vlookup.xlsx'
 sheetLeft = 'sheet1'
@@ -23,13 +25,11 @@ joinColumns = ['客户代码'] # 匹配列为复数列时['col1', 'col2']
 resultColumns = ['客户代码', 'SAP客户代码']
 fileName4Result = 'result.xlsx'
 sheetName4Result = 'result'
-# ★★★请自行修改相应参数★★★
-# ==============================================================================================
-# 左连匹配
 how = 'left'
+# ================================================================================================================
+# 主体部分，一般无需修改
 fileName = filePath + fileName
 fileName4Result = filePath + fileName4Result
-# ==============================================================================================
 
 if not os.path.exists(fileName):
     print('File not exist: ' + fileName)
